ef50b6b169c139e23b78008a6d459f821aa97547
BrijeshShinde7 <brijeshshinde2000@gmail.com>
2024-06-28 12:39:08 +0530

    Adding program.txt

---
+++ Program.java
@@ -0,0 +1,43 @@
+package linked_list;
+
+import java.util.Scanner;
+class Program{
+public static void main(String[] args) {
+    int ch, e;
+    Scanner sc = new Scanner(System.in);
+    CircularDoublyLinkedList obj = new CircularDoublyLinkedList();
+    obj.createList();
+    do {
+        System.out.println("\n1.Insert Left\n2.Insert Right\n3.Delete Left\n4.Delete Right\n5.Print List\n0.Exit\n:");
+        ch = sc.nextInt();
+        switch (ch) {
+            case 1:
+                System.out.println("Enter data:");
+                e = sc.nextInt();
+                obj.insertLeft(e);
+                break;
+            case 2:
+                System.out.println("Enter data:");
+                e = sc.nextInt();
+                obj.insertRight(e);
+                break;
+            case 3:
+                obj.deleteLeft();
+                break;
+            case 4:
+                obj.deleteRight();
+                break;
+            case 5:
+                obj.printList();
+                break;
+            case 0:
+                System.out.println("Exiting.....");
+                break;
+            default:
+                System.out.println("Wrong option selected");
+                break;
+        }
+    } while (ch != 0);
+}
+}
+
